b25212bbd0bfb53aae02a8eac51c6cc1b6adb3c6
Lalit <lalitprajapat9347@gmail.com>
2025-03-11 03:08:01 +0530

    creating food delivery app

--- App.js
+++ App.js
@@ -1,28 +1,47 @@
 import React from "react";
 import ReactDOM from "react-dom/client"
-const heading = React.createElement("h1", {id: "Heading"}, "Holla React"); //it takes 3 inputs tag ,{} where we give attributes to our tag , value we want to put inside tag
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-    
-// for nested elements like div inside div
-
-const p = React.createElement(
-    "div",
-     {id:"parent"}, 
-     React.createElement(
-        "div",
-         {id:"child"}, 
-         React.createElement(
-            "h1",
-            {},
-            "tag in nested div"
-        )
+import logo from "./image/logo.svg";
+const Header = () => {
+    return(
+        <div className="header">
+            <div className="logo">
+                <img src={logo} alt="logo-img"/>
+            </div>
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Contact Us</li>
+                    <li>cart</li>
+                </ul>
+            </div>
+        </div>
+    );
+};
+const RestaurantCard = () => {
+    return(
+        <div className="res-card" style={{backgroundColor:'#f0f0f0' }} >
+            <h3>EatFit</h3>
+        </div>
+    );
+};
+const Body = () =>{
+    return(
+       <div className="body">
+        <div className="search">Search</div>
+        <div className="res-container">
+            <RestaurantCard/>
+        </div>
+       </div> 
     )
-);
-const cont = React.createElement(
-    "div",
-    {id:"container"},
-    heading,
-    p
-);//as we can pass multiple children in 3rd or an array of childrens 
-root.render(cont);
+}
+const AppLayout = () => {
+    return(
+        <div className="app">
+            <Header/>
+            <Body/>
+        </div>
+    );
+};
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppLayout/>);
